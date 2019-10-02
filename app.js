9b81ce6dc0430ccf73a41282b75c8a95e1ddf211
Tejas Katyarmal <tkatyarmal@uk-c02tvca4htd6.uk.deloitte.com>
2019-10-02 15:42:00 +0100

    introduction to d3

--- app.js
+++ app.js
@@ -2,42 +2,47 @@ const height=500;
 const width=800;
 const padding = 50;
 
+// seelct DOM elements by id
+const graph = d3.select('#scatterplot')
+const plot = graph.selectAll('circle')
+
+const scaleX = d3.scaleLinear()
+  .domain(d3.extent(regionData,d=> d.medianAge))
+  .range([padding , width-padding])
+
+const scaleY = d3.scaleLinear()
+  .domain(d3.extent(regionData,d=> d.urbanPopulationRate))
+  .range([padding , height-padding])
+
+const scaleColour = d3.scaleLinear()
+  .domain (d3.extent(regionData,d=> d.extremePovertyRate))
+  .range (['yellow','green'])
 
 
 
+const col = d => scaleColour(d.extremePovertyRate)
+const xPos = d => scaleX(d.medianAge)
+const yPos = d => scaleY(d.urbanPopulationRate)
 
 
+  plot.data(regionData)
+    .enter()
+    .append('svg:circle')
+    .attr('r',5)
+    .attr('cx',xPos)
+    .attr('cy',yPos)
 
+    .style ('fill', col)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    .on ('mouseover', d => showTooltip (d))
+    .on ('mouseout', d => removeTooltips ())
 
 
 
 // tooltips are for the end of the demo
 
-showTooltip = () => {
-  const d = { data: `anything` }
-  const tooltipText = ``;
+showTooltip = d => {
+  const tooltipText = d.region;
   const tooltip = d3.select('body')
       .append("div")
 
@@ -57,3 +62,16 @@ removeTooltips = () => {
       .selectAll(".tooltip")
         .remove();
 }
+
+
+
+var x_axis = d3.axisBottom()
+  .scale(scaleX)
+  // .attr('x', padding)
+
+var y_axis = d3.axisLeft()
+  .scale(scaleY)
+  // .attr('y', padding)
+
+graph.append("svg")
+  .call(x_axis)
